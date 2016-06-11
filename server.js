5abe2b82c6ffb31d29f81290853acb3ecbd9cc66
Madhur Shrimal <madhur.shrimal@gmail.com>
2016-06-11 13:41:12 -0700

    updated routes

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.get('/', function(req, res){
 // REST call for getting sleep data
 app.get('/sleep', function(req, res){
 	res.setHeader('Content-Type', 'application/json');
-	
 	// get data from fitbit
 	var sleepdata = {
 		"sleep_duration": 436
@@ -43,16 +42,92 @@ app.get('/sleep', function(req, res){
     res.send(JSON.stringify(sleepdata));
 });
 
+// REST call for getting activity
+app.get('/activity', function(req, res){
+	res.setHeader('Content-Type', 'application/json');
+	var result = {
+		steps: 5054,
+		miles: 4.5,
+		calories: 2222,
+		floors: 10,
+		active_minutes: 45
+	};
+    res.send(JSON.stringify(result));
+});
+
+// REST call for getting frinds
+app.get('/friends', function(req, res){
+	res.setHeader('Content-Type', 'application/json');
+	
+	var result = {
+		data: [
+			{
+				name: "Ambuj",
+				steps: 4090,
+				miles: 4.5
+			},
+			{
+				name: "Suar",
+				steps: 5490,
+				miles: 5.5
+			}
+		]
+	};
+    res.send(JSON.stringify(result));
+});
+
 // REST call for getting calendar
 app.get('/calendar', function(req, res){
 	res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ action: "calendar"}));
+	
+	var result = {
+		data: [
+			{
+				name: "Meeting with Bhawna",
+				start: "09:00",
+				end: "10:00",
+
+			},
+			{
+				name: "All hands",
+				start: "11:00",
+				end: "12:00",
+
+			}
+		]
+	};
+    res.send(JSON.stringify(result));
 });
 
 // REST call for getting best route
 app.get('/route', function(req, res){
 	res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ action: "route"}));
+    var result = {
+		data: [
+			{
+				start_point: "140 Pasito Terrace",
+				end_point: "1 Infinite Loop, Cupertino",
+				distance: 5.4,
+				time: 44
+
+			},
+			{
+				start_point: "140 Pasito Terrace",
+				end_point: "1 Infinite Loop, Cupertino",
+				distance: 6.4,
+				time: 50
+
+			},
+			{
+				start_point: "140 Pasito Terrace",
+				end_point: "1 Infinite Loop, Cupertino",
+				distance: 3.4,
+				time: 45
+
+			}
+		]
+	};
+    res.send(JSON.stringify(result));
 });
 
 
