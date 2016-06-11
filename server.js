335441950c163717a51f79088c39d401b84d7c62
Madhur Shrimal <madhur.shrimal@gmail.com>
2016-06-11 11:15:20 -0700

    basic node server

--- server.js
+++ server.js
@@ -5,22 +5,51 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var socket = io;
 
+var WebSocketServer = require('ws').Server;
+
+var port = 8888; 
+
+var wss = new WebSocketServer({port: port});
+
+
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        ws.send('echo: ' + message);
+    });
+    ws.send('connected');
+});
+ 
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// REST call for getting sleep data
 app.get('/', function(req, res){
-	console.log("Defualt URL");
-}
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ action: "default"}));
+});
 
 // REST call for getting sleep data
 app.get('/sleep', function(req, res){
-
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ action: "sleep"}));
 });
 
 // REST call for getting calendar
 app.get('/calendar', function(req, res){
-
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ action: "calendar"}));
 });
 
 // REST call for getting best route
 app.get('/route', function(req, res){
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ action: "route"}));
+});
 
+
+http.listen(80, function(){
+   console.log('listening on *:80');
 });
 
